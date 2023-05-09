4e255fe520aafd22399b1c53e3995803b17f1d02
Johanna Avila <joeannaavila123@gmail.com>
2023-05-09 17:51:22 -0500

    fixing pycodestyle

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """print shape of matrix"""
+
+
 def matrix_shape(matrix):
-    shape = [] 
+    shape = []
     while isinstance(matrix, list):
-        shape.append(len(matrix))  # Append the length of the current dimension to the shape list
-        matrix = matrix[0]  # Move to the first element of the current dimension
+        shape.append(len(matrix))  # Add len of latest dimension 2 shape list
+        matrix = matrix[0]  # Move to the 1st element of the current dimension
     return shape
