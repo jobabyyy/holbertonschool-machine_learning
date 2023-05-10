b8c92467e4068ac7e81bf776a0252e8a08147088
Johanna Avila <joeannaavila123@gmail.com>
2023-05-09 19:26:38 -0500

    fixing pycode

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -7,13 +7,13 @@ def add_arrays(arr1, arr2):
 
     if len(arr1) != len(arr2):
         return None
-    
+
     # Create a new array to hold the sums
     result = []
-    
-    # Add the corresponding elements of the input arrays and append the sums to the result array
+
+    # Add elements of the input arrays & append the sums
     for i in range(len(arr1)):
         result.append(arr1[i] + arr2[i])
-    
+
     # Return the result array
     return result
