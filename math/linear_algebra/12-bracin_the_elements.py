8735acab814dd17335eaf66c42e3c3a05896d23c
Johanna Avila <joeannaavila123@gmail.com>
2023-05-11 18:55:53 -0500

    doing it all today

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""perfroms mult, division, add"""
+
+def np_elementwise(mat1, mat2):
+    """printing results"""
+    sum_result = mat1 + mat2
+    diff_result = mat1 - mat2
+    prod_result = mat1 * mat2
+    quotient_result = mat1 / mat2
+    return sum_result, diff_result, prod_result, quotient_result
