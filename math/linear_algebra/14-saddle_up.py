b96e0497bad8fb9c894c8e3b1138c961b593dc4a
Johanna Avila <joeannaavila123@gmail.com>
2023-05-11 19:23:32 -0500

    multiplying matrix

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""performing matrix multiplication"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """return new matrix"""
+    return np.matmul(mat1, mat2)
