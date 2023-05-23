6e9ec9b0bd759441954afac521f8b0d292bbe64c
Johanna Avila <joeannaavila123@gmail.com>
2023-05-23 16:34:11 -0500

    calc num of cdf

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -29,3 +29,10 @@ class Exponential:
             return 0
         else:
             return self.lambtha * 2.7182818285 ** (-self.lambtha * x)
+
+    def cdf(self, x):
+        """calc the value of CDF for given time"""
+        if x < 0:
+            return 0
+        else:
+            return 1 - 2.7182818285 ** (-self.lambtha * x)
