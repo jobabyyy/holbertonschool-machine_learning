45cbae2173a40378a4253e119024fdded93489b4
Johanna Avila <joeannaavila123@gmail.com>
2023-05-23 14:34:10 -0500

    calc value of pmf

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-"""class to rep a poisson distribution"""
+"""Class to represent a Poisson distribution"""
+
+import math
 
 
 class Poisson:
-    """expecting certain num of occurences"""
+    """Expecting a certain number of occurrences"""
+
     def __init__(self, data=None, lambtha=1.):
         if data is None:
             if lambtha <= 0:
@@ -17,7 +20,25 @@ class Poisson:
             self.lambtha = self.calculate_lambtha(data)
 
     def calculate_lambtha(self, data):
-        """calculating lambtha"""
+        """Calculating lambtha"""
         total = sum(data)
         num_points = len(data)
         return float(total) / num_points
+
+    def pmf(self, k):
+        k = int(k)
+        if k < 0:
+            return 0
+        else:
+            return (
+                (self.lambtha ** k) *
+                math.exp(-self.lambtha) /
+                self.factorial(k)
+            )
+
+    @staticmethod
+    def factorial(n):
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
