686c5b678bd4844bfd30b9728328782009795b6a
Johanna Avila <joeannaavila123@gmail.com>
2023-05-23 17:57:55 -0500

    ughhhh

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""class Normal to rep normal distr"""
 
 
 class Normal:
-    """normal distribution"""
+    """Normal distribution"""
     def __init__(self, data=None, mean=0., stddev=1.):
         """Initialize a Normal distribution."""
         if data is None:
@@ -27,27 +26,37 @@ class Normal:
         return mean, stddev
 
     def z_score(self, x):
-        """calc z score"""
+        """Calculate the z score"""
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
-        """calc x value of given z-score"""
+        """Calculate the x value of a given z-score"""
         return z * self.stddev + self.mean
 
     def pdf(self, x):
-        """calc val of given x value"""
+        """Calc val of the probability density function at x value"""
         exponent = -0.5 * ((x - self.mean) / self.stddev) ** 2
         denominator = self.stddev * (2.50662827463)
         return (1 / denominator) * (2.718281828459045) ** exponent
 
     def cdf(self, x):
-        """calc cdf"""
+        """Calculate the cumulative distribution function"""
         z = self.z_score(x)
-        return (1 + self.approx_erf(z / (2 ** 0.5))) / 2
+        return (1 + self._approx_erf(z / (2 ** 0.5))) / 2
 
     def _approx_erf(self, x):
-        """appro the error func"""
-        a1 =  0.254829592
+        """Approximation of the error function"""
+        a1 = 0.254829592
         a2 = -0.284496736
-        a3 =  1.421413741
+        a3 = 1.421413741
         a4 = -1.453152027
+        a5 = 1.061405429
+        p = 0.3275911
+
+        sign = 1 if x >= 0 else -1
+        x = abs(x)
+
+        t = 1.0 / (1.0 + p * x)
+        y = (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t
+
+        return sign * (1 - y * 2 ** (-1 * x * x))
