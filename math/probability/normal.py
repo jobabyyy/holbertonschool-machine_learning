0a41450523ba8e665d21fee2dab2da1d7e75860e
Johanna Avila <joeannaavila123@gmail.com>
2023-05-24 13:31:36 -0500

    pycodestyle fix

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -33,7 +33,7 @@ class Normal:
     def x_value(self, z):
         """Calculate the x value of a given z-score"""
         return z * self.stddev + self.mean
-    
+
     def pdf(self, x):
         """Calculates value of PDF for given x-value"""
         coefficient = 1 / (self.stddev * (2 * 3.1415926536) ** 0.5)
