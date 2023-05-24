3716642766f53dee9c2761dc2fb0704ecabf33e7
Johanna Avila <joeannaavila123@gmail.com>
2023-05-24 13:42:52 -0500

    updating values

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -17,8 +17,8 @@ class Binomial:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            # Assuming that all values in the data are equal to the number of successes,
+            # Assuming that all values in data are = to the num of successes,
             # we can find n by taking the maximum value in the data.
-            self.n = max(data)
-            # And p is the mean of the data divided by n.
+            self.n = int(max(data))
+            # And p is the average of the data divided by n.
             self.p = sum(data) / (self.n * len(data))
