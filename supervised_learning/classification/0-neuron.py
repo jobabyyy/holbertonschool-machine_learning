f6bd97e43ba06c9df59ab1292f23e053a54bd28d
Johanna Avila <joeannaavila123@gmail.com>
2023-06-06 16:54:17 -0500

    self.w is now 2 dimensional

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -6,11 +6,12 @@ import numpy as np
 
 class Neuron:
     def __init__(self, nx):
+        """def self & nx"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.W = np.random.normal(size=(nx,))
+        self.W = np.random.normal(size=(nx, 1))
         self.b = 0
         self.A = 0
