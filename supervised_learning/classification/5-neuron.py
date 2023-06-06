2d13603d41d1a3d6b58cb3bb1d476af70047f83e
Johanna Avila <joeannaavila123@gmail.com>
2023-06-06 17:29:08 -0500

    method gradient descent added

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""class Neuron continued..."""
+
+import numpy as np
+
+
+class Neuron:
+    """class Neuron defined"""
+    def __init__(self, nx):
+        """init self and nx"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """method to calc fwd propogation"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """calc cost"""
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        )
+        return cost
+
+    def evaluate(self, X, Y):
+        """method evaluate"""
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """method gradient descent"""
+        m = Y.shape[1]
+        dz = A - Y
+        dw = (1 / m) * np.matmul(dz, X.T)
+        db = (1 / m) * np.sum(dz)
+        self.__W -= alpha * dw
+        self.__b -= alpha * db
