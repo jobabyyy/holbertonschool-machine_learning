b3b9b8bb8bc7fcaa5cb936d4dfec8ff26ff41006
Johanna Avila <joeannaavila123@gmail.com>
2023-06-06 17:25:28 -0500

    method evaluate added

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""class Neuron continued.."""
+
+import numpy as np
+
+
+class Neuron:
+    """class Neuron"""
+    def __init__(self, nx):
+        """init self and nx"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """calc fwd propagation"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """calc cost"""
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        )
+        return cost
+
+    def evaluate(self, X, Y):
+        """method evaluation"""
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
