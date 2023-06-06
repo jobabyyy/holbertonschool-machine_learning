e58d9b6923f7b5e5ca82201d4cc58e1f4ac12726
Johanna Avila <joeannaavila123@gmail.com>
2023-06-06 17:15:30 -0500

    documenting

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -30,6 +30,7 @@ class Neuron:
         return self.__A
 
     def forward_prop(self, X):
+        """class method forward prop"""
         Z = np.matmul(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A
