93471cab27c53ef36af8b8eb2e73d84fc4f3a6d4
Johanna Avila <joeannaavila123@gmail.com>
2023-06-06 16:56:58 -0500

    adddoc

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -5,6 +5,7 @@ import numpy as np
 
 
 class Neuron:
+    """def a single neuron"""
     def __init__(self, nx):
         """def self & nx"""
         if not isinstance(nx, int):
@@ -12,6 +13,6 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.W = np.random.normal(size=(nx, 1))
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
