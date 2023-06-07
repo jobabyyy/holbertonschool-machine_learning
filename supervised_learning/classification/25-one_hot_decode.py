ac611eda48376844684534694d287fc05292987f
Johanna Avila <joeannaavila123@gmail.com>
2023-06-07 18:44:13 -0500

    changed axis to 0

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -7,7 +7,7 @@ import numpy as np
 def one_hot_decode(one_hot):
     """coverts into vector of labels"""
     try:
-        labels = np.argmax(one_hot.T, axis=1)
+        labels = np.argmax(one_hot.T, axis=0)
         return labels
     except Exception:
         return None
