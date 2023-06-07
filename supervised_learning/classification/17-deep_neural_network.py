e69a5c1d0622606dd4444bcd0415b5126b9b518e
Johanna Avila <joeannaavila123@gmail.com>
2023-06-07 16:29:23 -0500

    properties added

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Deep Neural Network class"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Deep Neural network performing binary classification"""
+    def __init__(self, nx, layers):
+        """Init self, nx, and layers"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if min(layers) <= 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for lix, layer_size in enumerate(layers, 1):
+            if type(layer_size) is not int:
+                raise TypeError("layers must be a list of positive integers")
+            w = np.random.randn(layer_size, nx) * np.sqrt(2/nx)
+            self.__weights["W{}".format(lix)] = w
+            self.__weights["b{}".format(lix)] = np.zeros((layer_size, 1))
+            nx = layer_size
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
