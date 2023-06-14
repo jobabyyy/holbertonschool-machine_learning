c8ebb0e0b7c3debff81a7de93885b1902948c610
Johanna Avila <joeannaavila123@gmail.com>
2023-06-14 16:31:00 -0500

    place holder updated

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 def create_placeholders(nx, classes):
     """placeholders for data and lables"""
-    x = tf.keras.Input(shape=(nx,), name='x')
-    y = tf.keras.Input(shape=(classes,), name='y')
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
 
     return x, y
