375a6b68208ad392c12cb2c2e43151c880894669
Johanna Avila <joeannaavila123@gmail.com>
2023-06-14 17:36:22 -0500

    calc loss

---
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""calc the softmax loss of a prediction"""
+
+import tensorflow as tf
+
+
+def calculate_loss(y, y_pred):
+    """using args y, y_pred to obtain tensor
+    containing the loss of the prediction"""
+
+    loss = tf.reduce_mean(tf.nn_softmax_cross_entropy_with_logits
+                          (labels=y, logits=y_pred))
+
+    return loss
