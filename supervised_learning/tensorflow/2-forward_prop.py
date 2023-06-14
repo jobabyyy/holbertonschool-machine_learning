28eba75e8dce0cab9334bd8a5294ce3395108414
Johanna Avila <joeannaavila123@gmail.com>
2023-06-14 17:10:19 -0500

    fwd prop

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -4,11 +4,10 @@
 import tensorflow as tf
 create_layer = __import__('1-create_layer').create_layer
 
+
 def forward_prop(x, layer_sizes=[], activations=[]):
     """fwd prop graph created"""
-    prev = x
-
     for i in range(len(layer_sizes)):
-        prev = create_layer(prev, layer_sizes[i, activations[i]])
+        x = create_layer(x, layer_sizes[i], activations[i])
 
-    return prev
+    return x
