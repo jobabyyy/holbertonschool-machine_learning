e6a6c6ea91dfba64dbd79a2e381b574e1abe09f6
Johanna Avila <joeannaavila123@gmail.com>
2023-06-14 17:39:50 -0500

    syntax error fixed

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -8,7 +8,7 @@ def calculate_loss(y, y_pred):
     """using args y, y_pred to obtain tensor
     containing the loss of the prediction"""
 
-    loss = tf.reduce_mean(tf.nn_softmax_cross_entropy_with_logits
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits
                           (labels=y, logits=y_pred))
 
     return loss
