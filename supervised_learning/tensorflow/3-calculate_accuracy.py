9bfef697c5c47a889bd611a22ab7c0f9e9dd03d5
Johanna Avila <joeannaavila123@gmail.com>
2023-06-14 17:21:44 -0500

    calc accuracy

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""calc the accuracy of a prediciton"""
+
+import tensorflow as tf
+
+
+def calculate_accuray(y, y_pred):
+    """accuracy calculated"""
+    correct_predictions = tf.equal(tf.argmax(y, axis=1)
+                                   tf.argmax(y_pred, axis=1))
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions,
+                                      tf.float32))
+
+    return accuracy
