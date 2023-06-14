9c12c04beda0927454a41197cadb0dbb6e0c11e2
Johanna Avila <joeannaavila123@gmail.com>
2023-06-14 16:49:12 -0500

    layer created

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,8 +7,10 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """create layer in neural network.
     init of prev, n & activation"""
-    layer = tf.layers.dense(prev, n, activation=activation,
-                            kernel_initializer=init)
     init = tf.contrib.layers.variances_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=init, name='layer')
 
-    return layer
+    output = layer(prev)
+    
+    return output
