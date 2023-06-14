f6753f43e9b78166dd839c28d0ced0a5e6b0fdd6
Johanna Avila <joeannaavila123@gmail.com>
2023-06-14 16:53:59 -0500

    removed name and output prev

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,10 +7,9 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """create layer in neural network.
     init of prev, n & activation"""
-    init = tf.contrib.layers.variances_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(units=n, activation=activation,
-                            kernel_initializer=init, name='layer')
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.dense(prev, n, activation=activation,
+                            kernel_initializer=init)
 
-    output = layer(prev)
 
-    return output
+    return layer
