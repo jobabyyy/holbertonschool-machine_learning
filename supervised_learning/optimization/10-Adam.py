0b481a072c2db1f85cbd03e6ba2d1b1ba6b5a6cc
jobabyyy <joeannaavila123@gmail.com>
2023-06-27 16:55:33 -0700

    creating training op

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -6,9 +6,4 @@ in tensorflow using the Adam op"""
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """creating the training op"""
 
-    opt = tf.train.AdamOptimizer(learning_rate=alpha,
-                                 beta1=beta1,
-                                 beta2=beta2, epsilon=epsilon)
-    train_op = opt.minimize(loss)
-
-    return train_op
+    return tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon).minimize(loss)
