31d8eeadc2a9bd285a3d207f1d9b7a9ea924d6aa
jobabyyy <joeannaavila123@gmail.com>
2023-06-27 15:41:59 -0700

    pycodestyle fix

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -6,9 +6,7 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
     """updates using gradient descent
     returns tuple: updated var at the new moment."""
 
-
     v = beta1 * v + (1 - beta1) * grad
     var = var - alpha * v
-
 
     return var, v
