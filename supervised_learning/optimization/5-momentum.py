050e2cce0dc7ea6c17c301f1837cd9ea39447324
jobabyyy <joeannaavila123@gmail.com>
2023-06-27 15:40:14 -0700

    update var

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""func to update a var w momentum opt"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """updates using gradient descent
+    returns tuple: updated var at the new moment."""
+
+
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+
+
+    return var, v
