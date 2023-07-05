fccc2c94df8a343db307c28ec1ccb945380f7b61
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 16:57:45 -0700

    Refactor l2_reg_cost function and improve code readability

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -9,9 +9,4 @@ import tensorflow as tf
 def l2_reg_cost(cost):
     """calcs the cost of neural network w
     l2 reg"""
-
-    l2_regularizer_losses = tf.get_collection(
-        tf.GraphKeys.REGULARIZATION_LOSSES)
-    l2_cost = cost + tf.reduce_sum(l2_regularizer_losses)
-
-    return l2_cost
+    return (cost + tf.losses.get_regularization_losses())
