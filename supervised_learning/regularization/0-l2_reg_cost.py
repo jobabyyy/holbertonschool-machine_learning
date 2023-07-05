234cb25816719a650ae4314761360f056cea0930
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 16:28:27 -0700

    changed weight and calc

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -6,14 +6,14 @@ w l2 regularization"""
 import numpy as np
 
 
-def 12_reg_cost(cost, lambtha, weights, L, m):
+def l2_reg_cost(cost, lambtha, weights, L, m):
     """calc cost"""
     reg_term = 0
 
     for layer in range(1, L + 1):
-        weight = weight['W' + str(layer)]
+        weight = weights['W' + str(layer)]
         reg_term += np.sum(np.square(weight))
 
-    12_cost = cost + (lambtha / (2 * m)) * reg_term
+    l2_cost = cost + (lambtha / (2 * m)) * reg_term
 
-    return 12_cosr
+    return l2_cost
