d23feb7b96546699bb96aa72f027c9f26da11371
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 16:45:05 -0700

    updates weights and biases

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -11,19 +11,17 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """updates the weights and biases"""
     m = Y.shape[1]
 
-    for layer in range[L, 0, -1]:
-        A_prev = cache['A' + str(layer - 1)]
-        A = cache['A' + str(layer)]
-        W = weights['W' + str(layer)]
-        b = weights['b' + str(layer)]
+    dZ = cache['A' + str(L)] - Y
 
-        if layer == L:
-            dZ = A - Y
-        else:
-            dZ = np.matmul(W.T, dZ) * (1 - np.power(A, 2))
+    for i in range(L, 0, -1):
+        A_prev = cache['A' + str(i - 1)]
+        W = weights['W' + str(i)]
+        b = weights['b' + str(i)]
 
-        dW = np.matmul(dZ, A_prev.T) / m + (lambtha / m) * W
-        db = np.sum(dZ, axis=1, keepdims=True) / m
+        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        weights['W' + str(layer)] = W - alpha * dW
-        weights['b' + str(layer)] = b - alpha * db
+        dZ = np.matmul(W.T, dZ) * (1 - np.square(A_prev))
+
+        weights['W' + str(i)] -= alpha * dW
+        weights['b' + str(i)] -= alpha * db
