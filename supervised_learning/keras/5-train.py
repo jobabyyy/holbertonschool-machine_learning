bbc98b67ab779d6c91d7774516f798bee79f828a
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 19:49:29 -0700

    adding validation value

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""trains a model using gradient descent
+part2 continued..."""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels,
+                batch_size, epochs, verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient descent
+    and analyzes vaildation data"""
+    history = network.fit(data, labels, batch_size=batch_size, epochs=epochs,
+                          validation_data=validation_data, verbose=verbose,
+                          shuffle=shuffle)
+    return history
