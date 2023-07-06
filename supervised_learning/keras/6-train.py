58807e14d4316d6af8f060ba6757c64fded9b71c
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 19:57:30 -0700

    callbacks added

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""trains a model using gradient descent
+part3 continued..."""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels,
+                batch_size, epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient descent
+    and analyzes vaildation data"""
+    history = network.fit(data, labels, batch_size=batch_size, epochs=epochs,
+                          validation_data=validation_data, callbacks=callbacks,
+                          verbose=verbose,
+                          shuffle=shuffle)
+    return history
