901a8c2de5f7053a1bf60731ee60cb9168a9f574
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 19:54:23 -0700

    trailing whitespace oop

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -6,7 +6,7 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels,
-                batch_size, epochs, validation_data=None, 
+                batch_size, epochs, validation_data=None,
                 verbose=True, shuffle=False):
     """Trains a model using mini-batch gradient descent
     and analyzes vaildation data"""
