1098625358d8f6a0d39bdc75035495971950825f
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 20:24:45 -0700

    best save

--- supervised_learning/keras/8-train.py
+++ supervised_learning/keras/8-train.py
@@ -9,7 +9,8 @@ def train_model(network, data, labels,
                 batch_size, epochs, validation_data=None,
                 early_stopping=False, patience=0,
                 learning_rate_decay=False, alpha=0.1,
-                decay_rate=1,
+                decay_rate=1, save_best=False,
+                filepath=None,
                 verbose=True, shuffle=False):
     """updates function to also
     save the best iteration of the model"""
