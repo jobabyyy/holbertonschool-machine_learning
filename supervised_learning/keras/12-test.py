d8c21f46e4e6566a286a1ba1228871c3844981cf
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 20:41:09 -0700

    evaluate method used

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Function that test a neural network"""
+
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """tests a neural network using the follwing
+    values and returns the loss and accuracy of
+    a model"""
+
+    loss = accuracy = network.evaluate(data,
+                                       labels, verbose=verbose)
+
+    return loss, accuracy
