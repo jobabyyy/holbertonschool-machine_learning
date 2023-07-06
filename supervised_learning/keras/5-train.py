68a480263bc8c64ad7c264f5b2fef4bab90e468e
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 19:53:09 -0700

    validation is none

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -6,10 +6,11 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels,
-                batch_size, epochs, verbose=True, shuffle=False):
+                batch_size, epochs, validation_data=None, 
+                verbose=True, shuffle=False):
     """Trains a model using mini-batch gradient descent
     and analyzes vaildation data"""
     history = network.fit(data, labels, batch_size=batch_size, epochs=epochs,
-                          validation_data=None, verbose=verbose,
+                          validation_data=validation_data, verbose=verbose,
                           shuffle=shuffle)
     return history
