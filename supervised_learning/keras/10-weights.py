a373c1c88c8bced02194c799c5a20df2ad23c141
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 20:32:20 -0700

    saves weight and save

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""weights load and save added"""
+
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """Saves a model's weights"""
+    network.save_weights(filename, save_format=save_format)
+
+
+def load_weights(network, filename):
+    """Loads a model's weights"""
+    network.load_weights(filename)
