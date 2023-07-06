c5c93e4c36a88dd5d0ceb185ca5a38813466db4f
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 20:45:42 -0700

    making a prediction

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""func that makes a predicition
+using a neural network"""
+
+
+import tensorflow.keras as K
+
+def predict(network, data, verbose=False):
+    """Makes a prediction using a neural network
+    Returns: the prediction for the data"""
+    prediction = network.predict(data, verbose=verbose)
+
+    return prediction
