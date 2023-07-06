bd0a72262c12b1fa690ab57835c95fc97a095af5
jobabyyy <joeannaavila123@gmail.com>
2023-07-05 20:47:44 -0700

    pycode validation

--- supervised_learning/keras/13-predict.py
+++ supervised_learning/keras/13-predict.py
@@ -5,6 +5,7 @@ using a neural network"""
 
 import tensorflow.keras as K
 
+
 def predict(network, data, verbose=False):
     """Makes a prediction using a neural network
     Returns: the prediction for the data"""
