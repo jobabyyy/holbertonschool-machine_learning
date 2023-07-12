1c649ad16386d106c410245d4854349d2992605f
jobabyyy <joeannaavila123@gmail.com>
2023-07-12 15:59:41 -0700

    lets see if this will do it

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -8,36 +8,33 @@ import tensorflow.keras as K
 
 
 def lenet5(X):
-    """building a modified version
-    of LeNet-5"""
-    conv1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
-                            activation='relu',
-                            kernel_initializer='he_normal')(X)
-    pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+    """building a modified version of LeNet-5"""
+    init = K.initializers.he_normal(seed=None)
 
-    # Convolutional Layer 2
+    # convolutional layer 1
+    conv1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
+                            activation='relu', kernel_initializer=init)(X)
+
+    # Pooling
+    pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
     conv2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='valid',
                             activation='relu',
-                            kernel_initializer='he_normal')(pool1)
+                            kernel_initializer=init)(pool1)
     pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
-
-    # Flatten
     flatten = K.layers.Flatten()(pool2)
 
-    # Fully Connected Layer 1
+    # fully connected layer1
     fc1 = K.layers.Dense(units=120, activation='relu',
-                         kernel_initializer='he_normal')(flatten)
-
-    # Fully Connected Layer 2
+                         kernel_initializer=init)(lflatten)
+    # fully connected layer2
     fc2 = K.layers.Dense(units=84, activation='relu',
-                         kernel_initializer='he_normal')(fc1)
+                         kernel_initializer=init)(fc1)
+    # output layer
+    output = K.layers.Dense(units=10, activation='softmax',
+                            kernel_initializer=init)(fc2)
 
-    # Output Layer
-    output = K.layers.Dense(units=10, activation='softmax')(fc2)
-
-    model = K.models.Model(inputs=X, outputs=output)
-
-    # Compile the model
+    model = K.Model(inputs=X, outputs=output)
+    # model complied
     model.compile(optimizer=K.optimizers.Adam(),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
