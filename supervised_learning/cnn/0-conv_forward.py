3628d0e55f36c22e2a975211db97983e69a10457
jobabyyy <joeannaavila123@gmail.com>
2023-07-12 11:47:20 -0700

    updating padding AGAIN

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -4,16 +4,17 @@ convolutional layer of a neural network"""
 
 import numpy as np
 
+import numpy as np
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
-    """DOC"""
+    """"doc"""
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
     sh, sw = stride
 
     if padding == "same":
-        pad_h = int(np.ceil(((h_prev - 1) * sh - h_prev + kh) / 2))
-        pad_w = int(np.ceil(((w_prev - 1) * sw - w_prev + kw) / 2))
+        pad_h = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
+        pad_w = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
     else:
         pad_h, pad_w = 0, 0
 
@@ -24,20 +25,18 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     w_out = int((w_prev - kw + 2 * pad_w) / sw) + 1
 
     Z = np.zeros((m, h_out, w_out, c_new))
-
     for c in range(c_new):
         for h in range(h_out):
             for w in range(w_out):
-                for i in range(m):
-                    vert_start = h * sh
-                    vert_end = vert_start + kh
-                    horiz_start = w * sw
-                    horiz_end = horiz_start + kw
-
-                    a_slice_prev = A_prev_pad[
-                        i, vert_start:vert_end, horiz_start:horiz_end, :]
-                    Z[i, h, w, c] = np.sum(
-                        a_slice_prev * W[:, :, :, c]) + b[0, 0, 0, c]
+                vert_start = h * sh
+                vert_end = vert_start + kh
+                horiz_start = w * sw
+                horiz_end = horiz_start + kw
+                
+                a_slice_prev = A_prev_pad[
+                    :, vert_start:vert_end, horiz_start:horiz_end, :]
+                Z[:, h, w, c] = np.sum(
+                    a_slice_prev * W[:, :, :, c], axis=(1, 2, 3)) + b[0, 0, 0, c]
 
     A = activation(Z)
 
