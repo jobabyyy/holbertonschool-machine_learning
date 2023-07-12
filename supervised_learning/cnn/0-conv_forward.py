614597ec309bef615445335ea60a02b3bd95599c
jobabyyy <joeannaavila123@gmail.com>
2023-07-12 14:28:07 -0700

    fixing pep8 and documenting

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -2,15 +2,18 @@
 """function that performs forward propagation over a
 convolutional layer of a neural network"""
 
+
 import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
-    """"doc"""
+    """Perform forward propagation over a convolutional
+    layer of a neural network."""
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
     sh, sw = stride
 
+    # Padding
     if padding == "same":
         pad_h = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
         pad_w = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
@@ -24,6 +27,8 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     w_out = int((w_prev - kw + 2 * pad_w) / sw) + 1
 
     Z = np.zeros((m, h_out, w_out, c_new))
+
+    # Convolution operation
     for c in range(c_new):
         for h in range(h_out):
             for w in range(w_out):
@@ -31,11 +36,12 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
                 vert_end = vert_start + kh
                 horiz_start = w * sw
                 horiz_end = horiz_start + kw
-                
+
                 a_slice_prev = A_prev_pad[
                     :, vert_start:vert_end, horiz_start:horiz_end, :]
                 Z[:, h, w, c] = np.sum(
-                    a_slice_prev * W[:, :, :, c], axis=(1, 2, 3)) + b[0, 0, 0, c]
+                    a_slice_prev * W[:, :, :,
+                                     c], axis=(1, 2, 3)) + b[0, 0, 0, c]
 
     A = activation(Z)
 
