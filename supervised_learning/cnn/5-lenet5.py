50ed8aa5c2720c4632efb380313490c939aae4ab
jobabyyy <joeannaavila123@gmail.com>
2023-07-12 15:14:44 -0700

    return value

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -8,39 +8,30 @@ import tensorflow.keras as K
 
 
 def lenet5(X):
-    """Beginning convolutional layer(s)"""
+    # Convolutional layer 1
     conv1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
                             activation='relu',
                             kernel_initializer='he_normal')(X)
-
-    # pooling Layer 1
+    # Max pooling layer 1
     pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
-
-    # convolutional Layer 2
+    # Convolutional layer 2
     conv2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='valid',
                             activation='relu',
                             kernel_initializer='he_normal')(pool1)
-
-    # pooling Layer 2
+    # Max pooling layer 2
     pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
-
-    # flatten prev output for fully connected layers
-    flat = K.layers.Flatten()(pool2)
-
-    # connected Layer 1
+    # Flatten the input for fully connected layers
+    flatten = K.layers.Flatten()(pool2)
+    # Fully connected layer 1
     fc1 = K.layers.Dense(units=120, activation='relu',
-                         kernel_initializer='he_normal')(flat)
-
-    # connected Layer 2
+                         kernel_initializer='he_normal')(flatten)
+    # Fully connected layer 2
     fc2 = K.layers.Dense(units=84, activation='relu',
                          kernel_initializer='he_normal')(fc1)
-
-    # setting output layer
+    # Output softmax layer
     output = K.layers.Dense(units=10, activation='softmax')(fc2)
 
-    model = K.models.Model(inputs=X, outputs=output)
-
-    """compiling the model"""
+    model = K.Model(inputs=X, outputs=output)
     model.compile(optimizer='adam', loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
