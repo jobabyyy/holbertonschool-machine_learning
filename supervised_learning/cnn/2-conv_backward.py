f38dc3a99c6f7bb646e34154fca1d52e10192859
jobabyyy <joeannaavila123@gmail.com>
2023-07-12 14:51:07 -0700

    changed order of loop

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -14,25 +14,30 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
 
     h_out = dZ.shape[1]
     w_out = dZ.shape[2]
-    
-    dA_prev = np.zeros_like(A_prev)
+
+    dA_prev = np.zeros((m, h_prev, w_prev, c_prev))
     dW = np.zeros_like(W)
-    db = np.zeros_like(b)
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
-    # computing dimensions of output volume after padding
+    # Padding
     if padding == "same":
-        pad_h = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
-        pad_w = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
+        pad_h = int(np.ceil((h_prev * sh - sh + kh - h_prev) / 2))
+        pad_w = int(np.ceil((w_prev * sw - sw + kw - w_prev) / 2))
+        A_prev_pad = np.pad(
+            A_prev,
+            ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
+            mode='constant')
+        dA_prev_pad = np.pad(
+            dA_prev,
+            ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
+            mode='constant')
     else:
-        pad_h, pad_w = 0, 0
-
-    A_prev_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h),
-                                 (pad_w, pad_w), (0, 0)), mode="constant")
-
-    h_out = int((h_prev - kh + 2 * pad_h) / sh) + 1
-    w_out = int((w_prev - kw + 2 * pad_w) / sw) + 1
+        A_prev_pad = A_prev
+        dA_prev_pad = dA_prev
 
     for i in range(m):
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
         for h in range(h_out):
             for w in range(w_out):
                 for c in range(c_new):
@@ -41,18 +46,15 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
                     horiz_start = w * sw
                     horiz_end = horiz_start + kw
 
-                    a_slice_prev = A_prev_pad[
-                        i, vert_start:vert_end, horiz_start:horiz_end, :]
-
-                    # computing gradients using the chain rule
-                    dA_prev[i, vert_start:vert_end,
-                            horiz_start:horiz_end, :] += W[
-                                           :, :, :, c] * dZ[i, h, w, c]
-                    dW[:, :, :, c] += a_slice_prev * dZ[i, h, w, c]
-                    db[:, :, :, c] += dZ[i, h, w, c]
-
-    # adjusting the gradients
-    if padding == "same":
-        dA_prev = dA_prev[:, pad_h:-pad_h, pad_w:-pad_w, :]
+                    a_slice = a_prev_pad[vert_start:vert_end,
+                                         horiz_start:horiz_end, :]
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end,
+                                :] += W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
+        # Gradients
+        if padding == "same":
+            dA_prev[i, :, :, :] = da_prev_pad[pad_h:-pad_h, pad_w:-pad_w, :]
+        else:
+            dA_prev[i, :, :, :] = da_prev_pad
 
     return dA_prev, dW, db
