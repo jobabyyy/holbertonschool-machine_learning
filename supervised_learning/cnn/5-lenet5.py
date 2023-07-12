a2e1506e6b7f7f18ce1edb23efe92be0ea56e383
jobabyyy <joeannaavila123@gmail.com>
2023-07-12 15:27:41 -0700

    structure

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -8,31 +8,38 @@ import tensorflow.keras as K
 
 
 def lenet5(X):
-    # Convolutional layer 1
+    """building a modified version
+    of LeNet-5"""
     conv1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
                             activation='relu',
                             kernel_initializer='he_normal')(X)
-    # Max pooling layer 1
     pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
-    # Convolutional layer 2
+
+    # Convolutional Layer 2
     conv2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='valid',
                             activation='relu',
                             kernel_initializer='he_normal')(pool1)
-    # Max pooling layer 2
     pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
-    # Flatten the input for fully connected layers
+
+    # Flatten
     flatten = K.layers.Flatten()(pool2)
-    # Fully connected layer 1
+
+    # Fully Connected Layer 1
     fc1 = K.layers.Dense(units=120, activation='relu',
                          kernel_initializer='he_normal')(flatten)
-    # Fully connected layer 2
+
+    # Fully Connected Layer 2
     fc2 = K.layers.Dense(units=84, activation='relu',
                          kernel_initializer='he_normal')(fc1)
-    # Output softmax layer
+
+    # Output Layer
     output = K.layers.Dense(units=10, activation='softmax')(fc2)
 
-    model = K.Model(inputs=X, outputs=output)
-    model.compile(optimizer='adam', loss='categorical_crossentropy',
+    model = K.models.Model(inputs=X, outputs=output)
+
+    # Compile the model
+    model.compile(optimizer=K.optimizers.Adam(),
+                  loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
     return model
