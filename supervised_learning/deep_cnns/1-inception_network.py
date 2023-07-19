64e9131504fdea129741231a4e218ef4cdeadaa7
Johanna Avila <joeannaavila123@gmail.com>
2023-07-19 16:09:28 -0500

    switching layers around a bit and changing structure

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -10,45 +10,45 @@ inception_block = __import__('0-inception_block').inception_block
 def inception_network():
     """model consisting of multiple inception
     blocks. It is designed for image classification.
-    func is to input data of shape given below and
+    func is to inputs data of shape given below and
     is to return inception Neural Network model. """
     # Input shape
-    X_input = K.Input(shape=(224, 224, 3))
+    inputs = K.Input(shape=(224, 224, 3))
 
-    # input layer
-    X = K.layers.Conv2D(64, (7, 7), strides=(2, 2),
-                        padding='same', activation='relu')(X_input)
-    X = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
+    # inputs layer
+    x = K.layers.Conv2D(64, (7, 7), strides=(2, 2),
+                        padding='same', activation='relu')(inputs)
+    x = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
 
     # conv layer num1
-    X = K.layers.Conv2D(192, (3, 3), activation='relu', padding='same')(X)
-    X = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
+    x = K.layers.Conv2D(192, (3, 3), activation='relu', padding='same')(x)
+    x = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
 
     # conv layer num2
-    X = inception_block(X, [64, 96, 128, 16, 32, 32])
-    X = inception_block(X, [128, 128, 192, 32, 96, 64])
-    X = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
+    x = inception_block(x, [64, 96, 128, 16, 32, 32])
+    x = inception_block(x, [128, 128, 192, 32, 96, 64])
+    x = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
 
     # inception blocks designed for image classification
-    X = inception_block(X, [192, 96, 208, 16, 48, 64])
-    X = inception_block(X, [160, 112, 224, 24, 64, 64])
-    X = inception_block(X, [128, 128, 256, 24, 64, 64])
-    X = inception_block(X, [112, 144, 288, 32, 64, 64])
-    X = inception_block(X, [256, 160, 320, 32, 128, 128])
-    X = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
+    x = inception_block(x, [192, 96, 208, 16, 48, 64])
+    x = inception_block(x, [160, 112, 224, 24, 64, 64])
+    x = inception_block(x, [128, 128, 256, 24, 64, 64])
+    x = inception_block(x, [112, 144, 288, 32, 64, 64])
+    x = inception_block(x, [256, 160, 320, 32, 128, 128])
+    x = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
 
     # inception 5a and 5b
-    X = inception_block(X, [256, 160, 320, 32, 128, 128])
-    X = inception_block(X, [384, 192, 384, 48, 128, 128])
+    x = inception_block(x, [256, 160, 320, 32, 128, 128])
+    x = inception_block(x, [384, 192, 384, 48, 128, 128])
 
     # final Layers
-    X = K.layers.AveragePooling2D(pool_size=(7, 7),
+    x = K.layers.AveragePooling2D(pool_size=(7, 7),
                                   strides=(7, 7),
-                                  padding='valid')(X)
-    X = K.layers.Dropout(0.4)(X)  # dropout
-    X = K.layers.Dense(1000, activation='softmax')(X)
+                                  padding='valid')(x)
+    x = K.layers.Dropout(0.4)(x)  # dropout
+    x = K.layers.Dense(1000, activation='softmax')(x)
 
     # create the model
-    model = K.models.Model(inputs=X_input, outputs=X)
+    model = K.models.Model(inputs=inputs, outputs=x)
 
     return model
