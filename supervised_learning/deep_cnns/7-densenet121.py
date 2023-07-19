2abb4b2e03ec9f7f49ced9712805d2e3a1236b71
Johanna Avila <joeannaavila123@gmail.com>
2023-07-19 18:21:41 -0500

    ok well apparently idk what im doin

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -13,44 +13,44 @@ def densenet121(growth_rate=32, compression=1.0):
     """Builds the DenseNet-121 architecture
     and returns Keras Model"""
 
-    X_inputs = K.Input(shape=(224, 224, 3))
+    inputs = K.Input(shape=(224, 224, 3))
 
     # convulational block inti
-    X_inputs = K.layers.BatchNormalization()(X_inputs)
-    X_inputs = K.layers.Activation('relu')(X_inputs)
-    X_inputs = K.layers.Conv2D(2 * growth_rate, (7, 7), strides=(2, 2),
+    x = K.layers.BatchNormalization()(inputs)
+    x = K.layers.Activation('relu')(x)
+    x = K.layers.Conv2D(2 * growth_rate, (7, 7), strides=(2, 2),
                         padding='same',
-                        kernel_initializer='he_normal')(X_inputs)
-    X_inputs = K.layers.MaxPooling2D((3, 3), strides=(2, 2),
-                              padding='same')(X_inputs)
+                        kernel_initializer='he_normal')(x)
+    x = K.layers.MaxPooling2D((3, 3), strides=(2, 2),
+                              padding='same')(x)
 
     # 1st dense block
-    X_inputs, nb_filters = dense_block(X_inputs, 2 * growth_rate, growth_rate, 6)
+    x, nb_filters = dense_block(x, 2 * growth_rate, growth_rate, 6)
 
     # 1st transition layer
-    X_inputs, nb_filters = transition_layer(X_inputs, nb_filters, compression)
+    x, nb_filters = transition_layer(x, nb_filters, compression)
 
     # 2nd dense block
-    X_inputs, nb_filters = dense_block(X_inputs, nb_filters, growth_rate, 12)
+    x, nb_filters = dense_block(x, nb_filters, growth_rate, 12)
 
     # 2nd transition layer
-    X_inputs, nb_filters = transition_layer(X_inputs, nb_filters, compression)
+    x, nb_filters = transition_layer(x, nb_filters, compression)
 
     # dense layer
-    X_inputs, nb_filters = dense_block(X_inputs, nb_filters, growth_rate, 24)
+    x, nb_filters = dense_block(x, nb_filters, growth_rate, 24)
 
     # 3rd transition layer
-    X_inputs, nb_filters = transition_layer(X_inputs, nb_filters, compression)
+    x, nb_filters = transition_layer(x, nb_filters, compression)
 
     # dense block
-    X_inputs, nb_filters = dense_block(X_inputs, nb_filters, growth_rate, 16)
+    x, nb_filters = dense_block(x, nb_filters, growth_rate, 16)
 
     # Pooling
-    X_inputs = K.layers.AveragePooling2D((7, 7))(X_inputs)
-    X_inputs = K.layers.Dense(1000, activation='softmax',
-                       kernel_initializer='he_normal')(X_inputs)
+    x = K.layers.AveragePooling2D((7, 7))(x)
+    x = K.layers.Dense(1000, activation='softmax',
+                       kernel_initializer='he_normal')(x)
 
     # Keras Model instance
-    model = K.Model(X_inputs=X_inputs, outputs=X_inputs)
+    model = K.Model(inputs=inputs, outputs=x)
 
     return model
