abf7daa2432fd4a642b54bbdc6e028ad691b5369
Johanna Avila <joeannaavila123@gmail.com>
2023-07-19 15:38:22 -0500

    adding padding to 2nd layer conv2

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -6,6 +6,7 @@ network"""
 import tensorflow.keras as K
 inception_block = __import__('0-inception_block').inception_block
 
+
 def inception_network():
     """model consisting of multiple inception
     blocks. It is designed for image classification.
@@ -22,20 +23,24 @@ def inception_network():
     x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
 
     # conv layer num2
-    x = K.layers.Conv2D(64, (1, 1), activation='relu')(x)
+    x = K.layers.Conv2D(64, (1, 1), padding='same', activation='relu')(x)
     x = K.layers.Conv2D(192, (3, 3), padding='same', activation='relu')(x)
     x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
 
     # inception blocks designed for image classification
     x = inception_block(x, [64, 96, 128, 16, 32, 32])  # inception 3a
     x = inception_block(x, [128, 128, 192, 32, 96, 64])  # inception 3b
+
     x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+
     x = inception_block(x, [192, 96, 208, 16, 48, 64])  # inception 4a
     x = inception_block(x, [160, 112, 224, 24, 64, 64])  # inception 4b
     x = inception_block(x, [128, 128, 256, 24, 64, 64])  # inception 4c
     x = inception_block(x, [112, 144, 288, 32, 64, 64])  # inception 4d
     x = inception_block(x, [256, 160, 320, 32, 128, 128])  # inception 4e
+
     x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+
     x = inception_block(x, [256, 160, 320, 32, 128, 128])  # inception 5a
     x = inception_block(x, [384, 192, 384, 48, 128, 128])  # inception 5b
 
