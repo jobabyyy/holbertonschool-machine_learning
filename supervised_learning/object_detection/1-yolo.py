b1584750d59c7c6119ea10fc998c657c21d932a7
Johanna Avila <joeannaavila123@gmail.com>
2023-08-21 17:50:24 -0500

    math stuff ugh

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-
 """
-Continued...
 Class Yolo that uses Yolo v3
 algorithm to perform object
 detection
+This class aims to create a base
+structure that will serve as the
+foundation for the following tasks.
 """
-
+import tensorflow.keras as K
 import numpy as np
-from tensorflow import keras as K
 
 
 class Yolo:
@@ -20,20 +20,16 @@ class Yolo:
     usage of YOLOv3 by encapsulating model loading,
     class info, and parameter config.
     """
-
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """Adding expected parameters
            Args:
                 model_path: path to pretained Yolo model
                 classes_path: list of class names
                 class_t: box score threshold for filtering
-                nms_t: IOU threshold for non max suppression
+                nnms_t: IOU threshold for non max suppression
                 anchors: anchor for box info
         """
         self.model = K.models.load_model(model_path)
-        self.model.compile(optimizer='adam',
-                           loss='categorical_crossentropy',
-                           metrics=['accuracy'])
         with open(classes_path) as file:
             class_names = file.read()
         self.class_names = class_names.replace("\n", "|").split("|")[:-1]
@@ -45,50 +41,48 @@ class Yolo:
         boxes = []
         box_confidences = []
         box_class_probs = []
-
-        for output in outputs:
+        for i, output in enumerate(outputs):
             grid_height, grid_width, anchor_boxes, _ = output.shape
-
+            box = np.zeros((grid_height, grid_width, anchor_boxes, 4))
             # Box coordinates adjustment
             box_tx = output[..., 0:1]
             box_ty = output[..., 1:2]
             box_tw = output[..., 2:3]
             box_th = output[..., 3:4]
-
-            # Using sigmoid function
-            box_tx_sigmoid = self.sigmoid(box_tx)
-            box_ty_sigmoid = self.sigmoid(box_ty)
-
-            # Create a grid of same shape as the predictions
-            grid = np.arange(grid_height).reshape(1, grid_height)
-            grid_x = np.tile(grid, [grid_width, 1]).T[...,
-                                                      np.newaxis,
-                                                      np.newaxis]
-            grid_y = np.tile(grid, [grid_height, 1])[...,
-                                                     np.newaxis,
-                                                     np.newaxis]
-
-            bx = box_tx / grid_width
-            by = box_ty / grid_height
-
-            bw = np.exp(box_tw) * self.anchors[:, 0] / grid_width
-            bh = np.exp(box_th) * self.anchors[:, 1] / grid_height
-
-            # Convert coordinates relative to the size of the image
-            x1 = (bx - (bw / 2)) * image_size[1]
-            y1 = (by - (bh / 2)) * image_size[0]
-            x2 = (bx + (bw / 2)) * image_size[1]
-            y2 = (by + (bh / 2)) * image_size[0]
-
-            # Box confidences and class probabilities
-            box_conf = self.sigmoid(output[..., 4:5])
+            # Get the anchors
+            pw = self.anchors[i, :, 0]
+            ph = self.anchors[i, :, 1]
+            # Calculate the real coordinates
+            bx = self.sigmoid(box_tx) + np.arange(
+                              grid_width).reshape(1, grid_width, 1)
+            by = self.sigmoid(box_ty) + np.arange(
+                              grid_height).reshape(grid_height, 1, 1)
+            bw = pw * np.exp(box_tw)
+            bh = ph * np.exp(box_th)
+            # Normalize the coordinates
+            bx /= grid_width
+            by /= grid_height
+            bw /= self.model.input.shape[1]
+            bh /= self.model.input.shape[2]
+            # Calculate the coordinates relative to the image size
+            x1 = (bx - bw / 2) * image_size[1]
+            y1 = (by - bh / 2) * image_size[0]
+            x2 = (bx + bw / 2) * image_size[1]
+            y2 = (by + bh / 2) * image_size[0]
+            # Update the box with the new coordinates
+            box[..., 0] = x1
+            box[..., 1] = y1
+            box[..., 2] = x2
+            box[..., 3] = y2
+            boxes.append(box)
+            # Get the confidences and class probabilities
+            box_confidence = self.sigmoid(output[..., 4])
+            box_confidences.append(
+                box_confidence.reshape(
+                    grid_height, grid_width, anchor_boxes, 1))
             box_class_prob = self.sigmoid(output[..., 5:])
-
-            boxes.append(np.stack([x1, y1, x2, y2], axis=-1))
-            box_confidences.append(box_conf)
             box_class_probs.append(box_class_prob)
-
-        return (boxes, box_confidences, box_class_probs)
+        return boxes, box_confidences, box_class_probs
 
     @staticmethod
     def sigmoid(x):
