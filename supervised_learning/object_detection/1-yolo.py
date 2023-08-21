81afeee318f317f070dde88afe116d3a2852c636
Johanna Avila <joeannaavila123@gmail.com>
2023-08-21 18:55:17 -0500

    Revert once again

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -3,8 +3,6 @@
 
 import numpy as np
 from tensorflow import keras as K
-
-
 class Yolo:
     """
     Yolo class uses algorithm Yolo v3 to complete
@@ -33,52 +31,44 @@ class Yolo:
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
-
     def process_outputs(self, outputs, image_size):
-        """process output"""
         boxes = []
         box_confidences = []
         box_class_probs = []
-
         for output in outputs:
             grid_height, grid_width, anchor_boxes, _ = output.shape
-            num_classes = output.shape[-1] - 5
-
-            box = output[..., :4]  # Extracting box coordinates and dimensions
-            box_conf = output[..., 4:5]  # Extracting box confidence
-            box_class_probs = output[..., 5:]  # Extracting class probabilities
-
-            # Applying sigmoid function to box_conf and box_class_probs
-            box_conf = self.sigmoid(box_conf)
-            box_class_probs = self.sigmoid(box_class_probs)
-
-            grid_x = np.arange(grid_width)
-            grid_y = np.arange(grid_height)
-
-            # Creating grid coordinates for x and y
-            grid_x, grid_y = np.meshgrid(grid_x, grid_y)
-
-            # Reshaping for calculations
-            grid_x = grid_x.reshape((-1, 1))
-            grid_y = grid_y.reshape((-1, 1))
-
-            # Box adjustments using sigmoid and anchors
-            box[:, :, :, 0] = (box[:, :, :, 0] + grid_x) / grid_width
-            box[:, :, :, 1] = (box[:, :, :, 1] + grid_y) / grid_height
-            box[:, :, :, 2] = np.exp(box[:, :, :, 2]) * self.anchors[:, 0]
-            box[:, :, :, 3] = np.exp(box[:, :, :, 3]) * self.anchors[:, 1]
-
-            # Converting box coordinates to image coordinates
-            box[:, :, :, 0:2] *= image_size[::-1]
-            box[:, :, :, 2:4] *= image_size[::-1]
-
-            # Stacking results
-            boxes.append(box)
+            # Box coordinates adjustment
+            box_tx = output[..., 0:1]
+            box_ty = output[..., 1:2]
+            box_tw = output[..., 2:3]
+            box_th = output[..., 3:4]
+            # Using sigmoid function
+            box_tx_sigmoid = self.sigmoid(box_tx)
+            box_ty_sigmoid = self.sigmoid(box_ty)
+            # Create a grid of same shape as the predictions
+            grid = np.arange(grid_height).reshape(1, grid_height)
+            grid_x = np.tile(grid, [grid_width, 1]).T[...,
+                                                      np.newaxis,
+                                                      np.newaxis]
+            grid_y = np.tile(grid, [grid_height, 1])[...,
+                                                     np.newaxis,
+                                                     np.newaxis]
+            box_x = box_tx_sigmoid + grid_x
+            box_y = box_ty_sigmoid + grid_y
+            box_w = np.exp(box_tw) * self.anchors[:, 0]
+            box_h = np.exp(box_th) * self.anchors[:, 1]
+            # Convert coordinates relative to the size of the image
+            x1 = (box_x - box_w / 2) / grid_width * image_size[1]
+            y1 = (box_y - box_h / 2) / grid_height * image_size[0]
+            x2 = (box_x + box_w / 2) / grid_width * image_size[1]
+            y2 = (box_y + box_h / 2) / grid_height * image_size[0]
+            # Box confidences and class probabilities
+            box_conf = self.sigmoid(output[..., 4:5])
+            box_class_prob = self.sigmoid(output[..., 5:])
+            boxes.append(np.stack([x1, y1, x2, y2], axis=-1))
             box_confidences.append(box_conf)
-            box_class_probs.append(box_class_probs)
-
-        return boxes, box_confidences, box_class_probs
-
+            box_class_probs.append(box_class_prob)
+        return (boxes, box_confidences, box_class_probs)
     @staticmethod
     def sigmoid(x):
         return 1 / (1 + np.exp(-x))
