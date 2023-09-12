4a458d5e4d692ca274a18f14a27ce6756de2ac52
Johanna Avila <joeannaavila123@gmail.com>
2023-09-11 22:12:08 -0600

    updating calc

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,46 +1,47 @@
 #!/usr/bin/env python3
-"""class multinormal"""
+"""Class Multinormal"""
 
 
 import numpy as np
 
 
 class MultiNormal:
-    """Multivariate distribution"""
+    """class multinormal"""
     def __init__(self, data):
-        if not isinstance(data, np.ndarray) or data.ndim != 2:
+        self.data = data
+
+        if not isinstance(self.data, np.ndarray) or self.data.ndim != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
 
-        d, n = data.shape
+        d, n = self.data.shape
 
         if n < 2:
             raise ValueError("data must contain multiple data points")
 
-        self.mean = np.mean(data, axis=1, keepdims=True)
+        self.mean = np.mean(self.data, axis=1, keepdims=True)
         self.cov = np.zeros((d, d))
 
         for i in range(d):
-            for j in range(i, d):
-                self.cov[i, j] = np.mean(
-                                         (data[i, :] - self.mean[i,
-                                          0]) * (data[j, :] - self.mean[j, 0]))
-                self.cov[j, i] = self.cov[i, j]
+            for j in range(d):
+                self.cov[i, j] = np.sum(
+                    (self.data[i] - self.mean[i]) * (
+                        self.data[j] - self.mean[j])) / (n - 1)
 
     def pdf(self, x):
-        """func to calc the PDF datapoint"""
+        """calculates the pdf"""
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy.ndarray")
 
-        d, _ = self.mean.shape
+        if x.shape != self.mean.shape:
+            raise ValueError(
+                "x must have the shape {}".format(self.mean.shape))
 
-        if x.shape != (d, 1):
-            raise ValueError(f"x must have the shape ({d}, 1)")
-
+        d = self.mean.shape[0]
+        det_cov = np.linalg.det(self.cov)
+        inv_cov = np.linalg.inv(self.cov)
         x_minus_mean = x - self.mean
-        exponent = -0.5 * np.dot(np.dot(x_minus_mean.T,
-                                 np.linalg.inv(self.cov)), x_minus_mean)
-        coefficient = 1 / ((2 * np.pi) ** (d /
-                                           2) * np.sqrt(np.linalg.det
-                                                        (self.cov)))
+        exponent = -0.5 * x_minus_mean.T @ inv_cov @ x_minus_mean
+        normalization = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(det_cov))
 
-        return coefficient * np.exp(exponent)
+        return normalization * np.exp(exponent)[0, 0]
+
