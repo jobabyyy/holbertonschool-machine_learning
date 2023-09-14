48c75a311845ff5f10e9c9fcc27ee06ce9443fe7
Johanna Avila <joeannaavila123@gmail.com>
2023-09-13 23:13:45 -0500

    updating classes

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -4,13 +4,12 @@
 
 def determinant(matrix):
     """Function to calculate the determinant of a matrix"""
-    # Check if the input is a list of lists
-    if not isinstance(matrix,
-                      list) or not all(isinstance(
-                                       row, list) for
-                                       row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance(
+                                               row, list)
+                                               for row in matrix):
         raise TypeError("matrix must be a list of lists")
-    # Check if the matrix is square
+    if matrix == [[]]:
+        return 1
     num_rows = len(matrix)
     if num_rows == 0:
         raise ValueError("matrix must be a square matrix")
@@ -19,16 +18,15 @@ def determinant(matrix):
         raise ValueError("matrix must be a square matrix")
     # Base cases for 0x0 and 1x1 matrices
     if num_rows == 0:
-        return 1  # The determinant of a 0x0 matrix is 1 by convention
+        return 1
     if num_rows == 1:
         return matrix[0][0]
-    # Initialize the determinant value
     det = 0
-    # Recursive calculation for larger matrices
+
     for col in range(num_columns):
         minor_matrix = [row[:col] + row[col + 1:] for row in matrix[1:]]
         cofactor = matrix[0][col] * determinant(minor_matrix)
-        det += cofactor * (-1) ** col  # Apply alternating signs
+        det += cofactor * (-1) ** col
 
     return det
 
