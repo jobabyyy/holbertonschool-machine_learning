35993ac7dbbc6c2fcb5fc12289c15367783b244f
Johanna Avila <joeannaavila123@gmail.com>
2023-09-13 22:19:10 -0500

    change back lol

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -60,7 +60,7 @@ def intersection(x, n, P, Pr):
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
-    if not np.all((P > 0) & (P < 1)):
+    if not np.all((P >= 0) & (P <= 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
     if not np.all((Pr >= 0) & (Pr <= 1)):
