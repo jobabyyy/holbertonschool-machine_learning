98fc938fe1002a4ce429348f97a3cba8dffe6f2b
Johanna Avila <joeannaavila123@gmail.com>
2023-09-13 21:18:03 -0500

    testing hypos

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Bayesian Probabiliy: Intersection"""
+
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """func to check likelihood"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0 or x > n:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    likelihoods = np.array([np.math.comb(
+                           n, x) * p**x * (1 - p)**(n - x) for p in P])
+
+    # Check if the result is a numpy.ndarray
+    if not isinstance(likelihoods, np.ndarray):
+        raise TypeError("The result must be a numpy.ndarray")
+
+    return likelihoods
+
+
+def intersection(x, n, P, Pr):
+    """tesing hypotheticals"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0 or x > n:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    # Check if Pr is a numpy.ndarray with the same shape as P
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P")
+
+    # Check if all values in P are in the range [0, 1]
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError(
+            f"All values in P must be in the range [0, 1]")
+
+    # Check if all values in Pr are in the range [0, 1]
+    if not np.all((Pr >= 0) & (Pr <= 1)):
+        raise ValueError(
+            f"All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1.0):
+        raise ValueError("Pr must sum to 1")
+
+    posterior = (Pr * P**x * (1 -
+                 P)**(n - x)) / np.sum(Pr * P**x * (1 - P)**(n - x))
+
+    return posterior
