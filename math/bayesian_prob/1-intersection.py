85dee2fd53b4e32bff09838ae688f1b377c097c6
Johanna Avila <joeannaavila123@gmail.com>
2023-09-13 22:06:58 -0500

    syntax errots

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -61,10 +61,10 @@ def intersection(x, n, P, Pr):
         raise TypeError("Pr must be a numpy.ndarray same shape as P")
 
     if not np.all((P >= 0) & (P <= 1)):
-        raise ValueError(f"All values in P must be in the range [0, 1]")
+        raise ValueError("All values in P must be in the range [0, 1]")
 
     if not np.all((Pr >= 0) & (Pr <= 1)):
-        raise ValueError(f"All values in Pr must be in the range [0, 1]")
+        raise ValueError("All values in Pr must be in the range [0, 1]")
     if not np.isclose(np.sum(Pr), 1.0):
         raise ValueError("Pr must sum to 1")
 
