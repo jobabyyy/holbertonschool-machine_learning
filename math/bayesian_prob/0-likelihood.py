0cb587dc036b482b32779bdc5b9f0712ec94994d
Johanna Avila <joeannaavila123@gmail.com>
2023-09-13 21:51:55 -0500

    cleaning it up and updating math.comb function

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -22,15 +22,17 @@ def likelihood(x, n, P):
     if not isinstance(x, int) or x < 0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
+
     if x > n:
-        raise ValueError("x cannot be a positive integer")
+        raise ValueError("x cannot be greater than n")
 
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
+
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    likelihoods = np.array([np.math.comb(
+    likelihoods = np.array([comb(
                            n, x) * p**x * (1 - p)**(n - x) for p in P])
 
     # Check if the result is a numpy.ndarray
