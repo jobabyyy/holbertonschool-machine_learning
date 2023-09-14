c154dee8d373adc12115971d2866db8afca3cff0
Johanna Avila <joeannaavila123@gmail.com>
2023-09-13 20:27:36 -0500

    pycodestyle

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -5,7 +5,9 @@
 def determinant(matrix):
     """Function to calculate the determinant of a matrix"""
     # Check if the input is a list of lists
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix,
+                      list) or not all(isinstance(row,
+                                       list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
     # Check if the matrix is square
     num_rows = len(matrix)
@@ -32,7 +34,9 @@ def determinant(matrix):
 
 def minor(matrix):
     """Calculate the minor matrix of a matrix."""
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix,
+                      list) or not all(isinstance(row,
+                                       list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
     num_rows = len(matrix)
     if num_rows == 0:
@@ -46,7 +50,8 @@ def minor(matrix):
         minor_row = []
         for j in range(num_columns):
             # Create a submatrix by excluding the i-th row and j-th column
-            submatrix = [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
+            submatrix = [row[:j] + row[j + 1:] for row in (
+                         matrix[:i] + matrix[i + 1:])]
             # Calculate the determinant of the submatrix
             minor_value = determinant(submatrix)
             minor_row.append(minor_value)
@@ -57,7 +62,10 @@ def minor(matrix):
 
 def cofactor(matrix):
     """Calculate the cofactor matrix of a matrix."""
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix,
+                      list) or not all(isinstance(row,
+                                       list) for row
+                                       in matrix):
         raise TypeError("matrix must be a list of lists")
     num_rows = len(matrix)
     if num_rows == 0:
@@ -69,7 +77,8 @@ def cofactor(matrix):
     for i in range(num_rows):
         cofactor_row = []
         for j in range(num_columns):
-            submatrix = [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
+            submatrix = [row[:j] + row[j + 1:] for row in
+                         (matrix[:i] + matrix[i + 1:])]
             minor_value = determinant(submatrix)
             cofactor_value = minor_value * (-1) ** (i + j)
             cofactor_row.append(cofactor_value)
