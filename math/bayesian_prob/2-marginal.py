f15a51a82de7ea057e7954b5698f80bc1f4f05dd
Johanna Avila <joeannaavila123@gmail.com>
2023-09-13 22:43:28 -0500

    testing n is not positive

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -95,10 +95,10 @@ def marginal(x, n, P, Pr):
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
     if not np.all((P >= 0) & (P <= 1)):
-        raise ValueError(f"All values in P must be in the range [0, 1]")
+        raise ValueError("All values in P must be in the range [0, 1]")
 
     if not np.all((Pr >= 0) & (Pr <= 1)):
-        raise ValueError(f"All values in Pr must be in the range [0, 1]")
+        raise ValueError("All values in Pr must be in the range [0, 1]")
 
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
