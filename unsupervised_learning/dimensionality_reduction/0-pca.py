0f3d385099ee50df81a9e875ecc47805172b79b3
Johanna Avila <joeannaavila123@gmail.com>
2023-09-25 16:29:41 -0500

    testing code

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Dimensionality Reduction:
+   Applying PCA on dataset."""
+
+
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """PCA performance on a dataset using Numpy.
+       Returns: the weights matrix 'W'
+       which maintains the specified fraction
+       of the OG variance."""
+    cov_matrix = np.cov(X, rowvar=False)
+
+    # Perform eigenvalue decomposition on the covariance matrix
+    eigvals, eigvecs = np.linalg.eig(cov_matrix)
+
+    # Sort eigenvalues and eigenvectors in descending order
+    sorted_indices = np.argsort(eigvals)[::-1]
+    eigvals = eigvals[sorted_indices]
+    eigvecs = eigvecs[:, sorted_indices]
+
+    # Determine target variance:
+    total_variance = np.sum(eigvals)
+    target_variance = var * total_variance
+
+    # Find min num of dimensions
+    cumulative_variance = np.cumsum(eigvals)
+    num_dimensions = np.argmax(cumulative_variance >= target_variance) + 1
+
+    # Extract principle components
+    W = eigvecs[:, :num_dimensions]
+
+    return W
