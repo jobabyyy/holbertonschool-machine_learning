fafe463d151e852d700a71e122c125feb9d4902e
Johanna Avila <joeannaavila123@gmail.com>
2023-09-27 18:31:40 -0500

    none added to k

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -18,7 +18,7 @@ def initialize(X, k):
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None
     if type(k) is not int or k <= 0:
-        return None
+        return None, None
     n, d = X.shape
 
     # calc min & max values
