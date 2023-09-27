883c68af341e1e5f203b7821553187a0cfb093ac
Johanna Avila <joeannaavila123@gmail.com>
2023-09-27 15:07:36 -0500

    ??????

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -12,24 +12,25 @@ def pca(X, var=0.95):
        which maintains the specified fraction
        of the OG variance."""
     cov_matrix = np.cov(X, rowvar=False)
-
     # Perform eigenvalue decomposition on the covariance matrix
     eigvals, eigvecs = np.linalg.eig(cov_matrix)
 
     # Sort eigenvalues and eigenvectors in descending order
-    sorted_indices = np.argsort(eigvals)[::-1]
+    sorted_indices = np.arange(0, len(eigvals), 1)
+    sorted_indices = ([x for  _, x in sorted(
+                      zip(eigvals, sorted_indices))])[::-1]
     eigvals = eigvals[sorted_indices]
     eigvecs = eigvecs[:, sorted_indices]
 
     # Determine target variance:
     total_variance = np.sum(eigvals)
-    target_variance = var * total_variance
+    exp_variance = eigvals * total_variance
 
     # Find min num of dimensions
-    cumulative_variance = np.cumsum(eigvals)
-    num_dimensions = np.argmax(cumulative_variance >= target_variance) + 1
+    cumulative_variance = np.cumsum(exp_variance)
+    num_dimensions = np.argmax(cumulative_variance >= exp_variance) + 1
 
     # Extract principle components
-    X = eigvecs[:, :num_dimensions]
+    W = eigvecs[:, :num_dimensions + 1]
 
-    return X
+    return W
