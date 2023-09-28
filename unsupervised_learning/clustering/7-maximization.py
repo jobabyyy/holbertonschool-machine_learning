68d51376c4726751e5f31c828dee7b177d7cb24f
Johanna Avila <joeannaavila123@gmail.com>
2023-09-27 22:16:50 -0500

    documenting

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -6,6 +6,19 @@ import numpy as np
 
 
 def maximization(X, g):
+    """"X is a numpy.ndarray of shape (n, d)
+    containing the data set
+g: is a numpy.ndarray of shape (k, n) containing
+   the posterior probabilities for each data
+   point in each cluster
+Returns: pi, m, S, or None, None,
+         None on failure
+pi: is a numpy.ndarray of shape (k,)
+    containing the updated priors for each cluster
+m: is a numpy.ndarray of shape (k, d)
+   containing the updated centroid means for each cluster
+S: is a numpy.ndarray of shape (k, d, d)
+   containing the updated covariance matrices for each cluster"""
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None
     if type(g) is not np.ndarray or len(g.shape) != 2:
