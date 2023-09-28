2f6e52d4566e34bbdba20760c8fc2dcf13f9a6f4
Johanna Avila <joeannaavila123@gmail.com>
2023-09-27 22:45:09 -0500

    plotting

---
+++ unsupervised_learning/clustering/12-agglomerative.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Clustering: Agg"""
+
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
+
+
+def agglomerative(X, dist):
+    """X is a numpy.ndarray of shape (n, d)
+    containing the dataset
+dist: is the maximum cophenetic distance
+      for all clusters
+      Performs agglomerative clustering with Ward
+      linkage
+Displays the dendrogram with each cluster
+displayed in a different color
+Returns: clss, a numpy.ndarray of shape (n,)
+containing the cluster indices for each data point
+"""
+    linkage_matrix = scipy.cluster.hierarchy.linkage(X, method='ward')
+    dendrogram = scipy.cluster.hierarchy.dendrogram(linkage_matrix,
+                                                    color_threshold=dist)
+
+    clss = scipy.cluster.hierarchy.fcluster(linkage_matrix,
+                                            t=dist,
+                                            criterion='distance')
+
+    plt.show()
+
+    return clss
