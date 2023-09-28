a2f634520384e5ad8ca77af98f3adfe83a4a2531
Johanna Avila <joeannaavila123@gmail.com>
2023-09-27 22:35:59 -0500

    documentation

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -8,7 +8,16 @@ maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """doc"""
+    """X is a numpy.ndarray of shape (n, d) containing the data set
+k is a positive integer containing the number of clusters
+iterations is a positive integer containing the maximum number
+of iterations for the algorithm
+tol is a non-negative float containing tolerance of the
+log likelihood, used to determine early stopping i.e. if
+the difference is less than or equal to tol you should
+stop the algorithm
+verbose is a boolean that determines if you should print
+information about the algorithm"""
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None, None, None
     if type(k) is not int or k <= 0:
