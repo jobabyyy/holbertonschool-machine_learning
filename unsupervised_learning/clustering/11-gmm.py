eeb1c581ffe2ca3e5528281e6d77866f6fe234b6
Johanna Avila <joeannaavila123@gmail.com>
2023-09-27 23:05:05 -0500

    calculates gmm from data set

---
+++ unsupervised_learning/clustering/11-gmm.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Clustering: GMM"""
+
+import sklearn.mixture
+
+
+def gmm(X, k):
+    """X is a numpy.ndarray of shape (n, d)
+    containing the dataset
+    k: is the number of clusters
+       The only import you are allowed to use
+       is import sklearn.mixture
+    Returns: pi, m, S, clss, bic
+    pi: is a numpy.ndarray of shape (k,)
+        containing the cluster priors
+    m:  is a numpy.ndarray of shape (k, d)
+        containing the centroid means
+    S:  is a numpy.ndarray of shape (k, d, d)
+        containing the covariance matrices
+    clss: is a numpy.ndarray of shape (n,)
+          containing the cluster indices
+          for each data point"""
+    gmm_model = sklearn.mixture.GaussianMixture(n_components=k).fit(X)
+    pi = gmm_model.weights_
+    m = gmm_model.means_
+    S = gmm_model.covariance_
+    clss = gmm_model.predict(X)
+    bic = gmm_model.bic(X)
+
+    return pi, m, S, clss, bic
