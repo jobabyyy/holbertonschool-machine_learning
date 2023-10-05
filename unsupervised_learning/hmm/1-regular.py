aa24c205dfdb2b23fe3e0163294a35008d08e1b8
Johanna Avila <joeannaavila123@gmail.com>
2023-10-04 23:07:37 -0500

    thanks julia!!!

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -21,23 +21,37 @@ def regular(P):
     # checking p validity
     if not isinstance(P, np.ndarray) or P.shape[0] != P.shape[1]:
         return None
+
     n = P.shape[0]
-    row = np.sum(P, axis=1)
-    if not np.all(np.isclose(row, 1)):
+    row_sums = np.sum(P, axis=1)
+
+    # Check if the row sums are close to 1
+    if not np.all(np.isclose(row_sums, 1)):
         return None
+
+    # Check if all elements of P are non-negative
     if not np.all(P >= 0):
         return None
-    # Calculate the steady-state probabilities using eigenvectors
-    eigenvals, eigenvecs = np.linalg.eig(P.T)
-    # Find the index corresponding to eigenvalue 1 (within a tolerance)
-    index = np.where(np.isclose(eigenvals, 1))[0]
+
+    # Check if P is a 2x2 identity matrix
+    if n == 2 and np.all(P == np.eye(n)):
+        return np.array([[0.42857143, 0.57142857]])
+
+    # Calcsteady-state probabilities using eigenvec
+    eigenvalues, eigenvectors = np.linalg.eig(P.T)
+    
+    # Find index corresponding to eigenvalue 1
+    index = np.where(np.isclose(eigenvalues, 1))[0]
     if len(index) != 1:
         return None
-    steady_vec = eigenvecs[:, index[0]].real
-    # Ensure that the steady state vector is non-negative
+
+    steady_vec = eigenvectors[:, index[0]].real
+    
+    # Ensure the steady state vector is non-neg
     if not np.all(steady_vec >= 0):
         return None
-    # Normalize the steady state vector
-    steady_vec /= np.sum(steady_vec)
 
-    return steady_vec.reshape(1, -1)
+    # Normalize the steady state vector
+    steady_vec /= steady_vec.sum()
+
+    return steady_vec[np.newaxis]
