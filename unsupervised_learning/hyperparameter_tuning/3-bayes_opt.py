bbf4ac6c308b36fe7209f206325c83acbf53a099
Johanna Avila <joeannaavila123@gmail.com>
2023-10-18 18:45:28 -0500

    fixing doc error

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -11,8 +11,6 @@ GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
-                 l=1, sigma_f=1, xsi=0.01, minimize=True):
         """
         f: black box function be optimized.
         X_init: numpy ndarray of shape (t, 1) reps the input
@@ -32,9 +30,11 @@ class BayesianOptimization:
              should be performed for minimization
              (True) or maximization (False).
         """
-        self.f = f
-        self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1],
+        def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+                self.f = f
+                self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
+                self.X_s = np.linspace(bounds[0], bounds[1],
                                num=ac_samples).reshape(-1, 1)
-        self.xsi = xsi
-        self.minimize = minimize
+                self.xsi = xsi
+                self.minimize = minimize
