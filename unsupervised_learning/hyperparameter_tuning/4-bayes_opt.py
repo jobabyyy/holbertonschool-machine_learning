ea44a77de692203f4f14ce7063d8dbbd955c008e
Johanna Avila <joeannaavila123@gmail.com>
2023-10-18 20:15:18 -0500

    ok now fixed frfr

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
 """Hyperparameter Tuning:
-Bayesian Optimization -
-Acquisition
+Bayesian Optimization - Acquisition
 """
 
-
 import numpy as np
-GP = __import__('2-gp').GaussianProcess
 from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
+    """
+    f: black box function be optimized.
+    X_init: numpy ndarray of shape (t, 1) reps the input
+            already sampled with the blackbox function.
+    Y_init: numpy ndarray of shape (t, 1) reps the output
+            of box function for each input in X_init
+    t: is the number of initial samples.
+    bounds: a tuple of (min, max) reps the bounds of the space
+            in which to look for the optimal point.
+    ac_samples: num of samples that should be analyzed during
+                acquisition.
+    l: is the length of the parameter for the kernel.
+    sigma_f: is the standard deviation given to the
+             output of the black-box function.
+    xsi: is the exploration-exploitation factor for acquisition
+         minimize is a bool determining whether optimization
+         should be performed for minimization
+         (True) or maximization (False).
+    """
     def __init__(self, f, X_init, Y_init, bounds, ac_samples,
                  l=1, sigma_f=1, xsi=0.01, minimize=True):
         """
-        f: black box function be optimized.
-        X_init: numpy ndarray of shape (t, 1) reps the input
-                already sampled with the blackbox function.
-        Y_init: numpy ndarray of shape (t, 1) reps the output
-                of box function for each input in X_init
-        t: is the number of initial samples.
-        bounds: a tuple of (min, max) reps the bounds of the space
-                in which to look for the optimal point.
-        ac_samples: num of samples that should be analyzed during
-                    acquistion.
-        l: is the length of the parameter for the kernal.
-        sigma_f: is the standard deviation given to the
-                 output of the black-box function.
-        xsi: is the exploration-exploitation factor for acquisition
-             minimize is a bool determining whether optimization
-             should be performed for minimization
-             (True) or maximization (False).
+        init
         """
         self.f = f
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
@@ -41,15 +42,14 @@ class BayesianOptimization:
 
     def acquisition(self):
         """
-
-       Uses the Expected Improvement acquisition function
-       Returns: X_next, EI
-       X_next: is a numpy.ndarray of shape (1,) representing
-               the next best sample point
-       EI: is a numpy.ndarray of shape (ac_samples,)
-           containing the expected improvement of each
-           potential sample
-       """
+        Uses the Expected Improvement acquisition function
+        Returns: X_next, EI
+        X_next: is a numpy.ndarray of shape (1,) representing
+                the next best sample point
+        EI: is a numpy.ndarray of shape (ac_samples,)
+            containing the expected improvement of each
+            potential sample
+        """
         mu, sigma = self.gp.predict(self.X_s)
 
         if self.minimize:
