599d75d2d9210fbbbfe219fd2db93b90e32b8d7d
Johanna Avila <joeannaavila123@gmail.com>
2023-11-08 17:54:00 -0600

    function bi_forward created, fwd prop needed

---
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""RNNs: BidirectionalCell.
+"""
+
+
+import numpy as np
+
+
+class BidirectionalCell():
+    """ Class BidirectionalCell
+    """
+    def __init__(self, i, h, o):
+        """ Class Bidirectional that
+            represents a bidirectional
+            cell of an RNN.
+
+        class constructor def __init__(self, i, h, o):
+        i: is the dimensionality of the data
+        h: is the dimensionality
+           of the hidden states
+        o: is the dimensionality of the outputs
+            - Creates the public instance
+              attributes Whf, Whb, Wy, bhf, bhb,
+              by that represent the weights
+              and biases of the cell
+        Whf and bhfare: for the hidden states
+            in the forward direction
+        Whb and bhbare: for the hidden states
+            in the backward direction
+        Wy and byare: for the outputs
+        """
+        self.Whf = np.random.normal(size=(h, h))
+        self.Whb = np.random.normal(size=(h, h))
+        self.Wy = np.random.normal(size=(h*2, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
