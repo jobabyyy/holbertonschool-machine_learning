56e5ebd4f19247f1a01bbde7fc4062bfef5e4961
Johanna Avila <joeannaavila123@gmail.com>
2023-12-04 16:30:01 -0600

    tokenized to only remove plural

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -13,7 +13,7 @@ def tokenize(sentence):
     using regex."""
 
     sentence = sentence.lower()
-    words = re.findall(r"\w+\b", sentence)
+    words = re.findall(r"\b\w+\b", sentence)
     return words
 
 
@@ -46,7 +46,7 @@ def bag_of_words(sentences, vocab=None):
 
     # Create vocabulary if not provided
     if vocab is None:
-        excluded_words = {"is", "s"} 
+        excluded_words = {"s"} 
         all_words = [word for sentence in tokenized_sentences
                      for word in sentence]  # flatten sentences
 
