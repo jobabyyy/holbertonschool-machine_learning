64cb4bd8778dc9a450c3dbb0e7385d8b0f117c23
Johanna Avila <joeannaavila123@gmail.com>
2023-12-04 17:14:07 -0600

    pycodestyle and cleaning up

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
+"""Function that creates and
+trains a genism fasttext Model:"""
 
 
 from gensim.models import FastText
 
-def fasttext_model(sentences, size=100, min_count=5, negative=5, window=5, cbow=True, epochs=5, seed=0, workers=1):
+
+def fasttext_model(sentences, size=100, min_count=5, negative=5,
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
     """
     Create and train a Gensim FastText model.
 
     Args:
         sentences: List of sentences to be trained on.
         size: Dimensionality of the embedding layer.
-        min_count: Minimum number of occurrences of a word for use in training.
+        min_count: Minimum number of occurrences of a
+                   word for use in training.
         negative: Size of negative sampling.
-        window: Maximum distance between the current and predicted word within a sentence.
-        cbow: Boolean to determine the training type (True for CBOW, False for Skip-gram).
+        window: Maximum distance between the current and
+                predicted word within a sentence.
+        cbow: Boolean to determine the training type
+              True for CBOW, False for Skip-gram).
         epochs: Number of training epochs.
         seed: Seed for the random number generator.
         workers: Number of worker threads to train the model.
